1641d57b679f52ada6824338a559d0e77a4da1ee
Abdelrahman <abdelrahmanawad.dev@gmail.com>
2023-11-09 16:29:47 +0200

    some edits in DB connection

--- Src/Modules/Orders/order.routes.js
+++ Src/Modules/Orders/order.routes.js
@@ -3,7 +3,7 @@ const router = Router();
 import { errorHandling } from "../../Utils/ErrorHandling.js";
 import { validationCoreFunction } from "../../MiddleWares/Validation.js";
 import * as oc from "./order.controller.js";
-import * as validators from "./order.validationSchema.js"
+import * as validators from "./order.validationSchema.js";
 import { isAuth } from "../../MiddleWares/Auth.js";
 import { orderApisRoles } from "./order.endpoints.js";
 
@@ -11,26 +11,20 @@ router.post(
     "/createOrder",
     isAuth(orderApisRoles.CREAT_ORDER),
     validationCoreFunction(validators.createOrderSchema),
-    errorHandling(oc.createOrder),
-)
+    errorHandling(oc.createOrder)
+);
 router.post(
     "/cartToOrder",
     isAuth(orderApisRoles.CART_TO_ORDER),
     validationCoreFunction(validators.cartToOrderSchema),
-    errorHandling(oc.cartToOrder),
-)
+    errorHandling(oc.cartToOrder)
+);
 
-router.get(
-    "/successOrder",
-    errorHandling(oc.successPayment),
-)
-router.get(
-    "/cancelOrder",
-    errorHandling(oc.cancelPayment),
-)
+router.get("/successOrder", errorHandling(oc.successPayment));
+router.get("/cancelOrder", errorHandling(oc.cancelPayment));
 router.post(
     "/orderDelivered",
     isAuth(orderApisRoles.ORDER_DELIVERED),
-    errorHandling(oc.orderDelivered),
-)
-export default router
+    errorHandling(oc.orderDelivered)
+);
+export default router;
